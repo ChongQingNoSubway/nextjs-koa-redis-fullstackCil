ad89d4ce728884be883467d372330778485b55f5
ChongQingNoSubway <zhuwenhiu@gmail.com>
2020-12-21 23:46:58 -0700

    lazycomponent-moment

--- pages/a.js
+++ pages/a.js
@@ -1,7 +1,9 @@
 import Comp from '../components/comp'
 import Link from 'next/link'
-import { withRouter} from 'next/router'
+import { withRouter } from 'next/router'
 import styled from 'styled-components'
+import dynamic from 'next/dynamic'
+// import moment from 'moment'
 // export default () => (
 //     <Link href="/index">
 //         <div><Comp>a</Comp></div>
@@ -9,22 +11,28 @@ import styled from 'styled-components'
 // )
 
 
+// import lazy from '../components/layout'
+
+const Lazy = dynamic(import('../components/lazy'))
+
 // style-component css
 const Title = styled.h1`
     color: yellow;
     font-size: 40px
 `
 
-const A = ({ router, name }) => {
+const A = ({ router, name, time }) => {
 
     return(
         <>
-        <Title>Hello World</Title>
+        <Title>{time}</Title>
         {/* <Link href="#aaa"></Link> hash route */}
         <Link href="/index">
         <div className="link"><Comp className="link">a{router.query.id}</Comp>{name}</div>
         </Link>
 
+        <Lazy/>
+        
         <style jsx>{`
             .link {
                 color: green;
@@ -38,10 +46,20 @@ const A = ({ router, name }) => {
 }
 
 A.getInitialProps = async () => {
+    // Asynchronous loading 
+    // Avoid some page do need to moment also loading it
+    // if we use import moment every js , it make the loading of modules is huge.
+    // Avoid module resource waste
+    const moment = await import('moment')
+
+
     const promise = new Promise((resolve) => {
         setTimeout(() => {
             resolve({
-                name: 'jacky'
+                name: 'jacky',
+                // if we want to use moment 
+                // must add default
+                time: moment.default(Date.now()-60*1000).fromNow(),
             })
         },1000)
     })
